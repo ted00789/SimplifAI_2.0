e6e65f131e235d914e58d1decf94fd527a36cc67
ted00789 <ted.manas@gmail.com>
2026-01-23 11:10:31 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+const redirect = sessionStorage.getItem("redirect");
+
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
